6ec24bcf1cd6985beff76291801674d0dd678479
Phred Lender <shemp9999@gmail.com>
2014-10-23 20:44:16 -0700

    Initial Commit with updated comments
    Added a header comment and included all grading requirements (to be
    removed as fulfilled and replaced with contextual comments related to
    code execution).
    
    This commit is solely for git command practice.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,135 @@
+# R Programming Assignment 2
+# Requires forked git repository (ProgrammingAssignment2)
+
+#####################################
+# original code follows:
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+# makeCacheMatrix <- function(x = matrix()) {
+#     
+# }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# cacheSolve <- function(x, ...) {
+#     ## Return a matrix that is the inverse of 'x'
+# }
+
+# end original code
+#####################################
+
+# Evaluation/feedback on the above work
+# 
+
+
+
+# Was a valid GitHub URL containing a git repository submitted?
+# 
+# 0 points: A valid GitHub URL was NOT submitted (or URL is broken)
+# 1 point: The submitted URL points to a GitHub repository
+# 
+
+
+
+# Does the GitHub repository contain at least one commit beyond the original fork?
+# 
+# 0 points: No, there are no commits beyond the original fork
+# 1 point: Yes, there is at least one commit beyond the original fork
+# 
+
+
+
+# Was a SHA-1 submitted indicating a specific commit in the GitHub repository?
+# 
+# 0 points: No, there was no SHA-1 submitted
+# 1 point: Yes, a SHA-1 was submitted
+# 
+
+
+
+# If a SHA-1 was submitted along with the GitHub URL, does the SHA-1 
+# correspond to a specific commit in the repository?
+# 
+# 0 points: No, there is no commit in the repository corresponding to that SHA-1
+# 2 points: Yes, there is a commit in the repository corresponding to that SHA-1
+#
+
+
+# Overall evaluation/feedback
+# 
+# Does the GitHub repository contain an R file containing code implementing 
+# the completed assignment? 
+# 
+# NOTE: There is no need to run the code here, but rather you should visually inspect 
+# the R file in the GitHub repository and check to see that there is code there beyond 
+# the original stub that was committed there by the instructor. Do not attempt to judge 
+# whether the code is correct or not.
+# 
+# 0 points: No, the R file does not contain any code implementing the completed assignment
+# 1 point:  The R file contains code implementing a partially completed assignment
+# 2 points: The R file contains code implementing the complete assignment
+# 
+
+
+
+# Does the R file containing the code have any comments explaining what the code does?
+# 
+# NOTE: The makeCacheMatrix and cacheSolve functions should both be documented with 
+# explanatory comments. There maybe other functions in the R file but they do not 
+# need to be commented.
+# 
+# 0 points: There are no explanatory comments in the R file
+# 1 points: One of the functions has some corresponding explanatory comments in the R file
+# 2 points: Both functions have corresponding explanatory comments in the R file
+# 
+
+
+
+# Does the R code implementing the 'makeCacheMatrix' function appear to be correct, 
+# to the best of your ability to judge?
+# 
+# NOTE: Do not run the R code on your own computer. Please examine the R code and 
+# determine to the best of your ability, whether the solution presented appears to match 
+# the requirements of the assignment. A correct solution (as best you can determine) 
+# gets 1 point and a solution that is well-written and easy to read gets an additional 1 point.
+# 
+# 0 points: The solution contains at least one identifiable problem
+# 1 point:  The solution appears to be correct
+# 2 points: The solution appears to be correct and is written in a well-formatted and 
+#           easily readable style
+# 
+
+
+
+# Does the R code implementing the 'cacheSolve' function appear to be correct, 
+# to the best of your ability to judge?
+# 
+# NOTE: Do not run the R code on your own computer. Please examine the R code and 
+# determine to the best of your ability, whether the solution presented appears to match 
+# the requirements of the assignment. A correct solution (as best you can determine) 
+# gets 1 point and a solution that is well-written and easy to read gets an additional 1 point.
+# 
+# 0 points: The solution contains at least one identifiable problem
+# 1 point:  The solution appears to be correct
+# 2 points: The solution appears to be correct and is written in a well-formatted and 
+#           easily readable style
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
